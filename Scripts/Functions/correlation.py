Deleted Old Website & Bug Variance

Scripts/Functions/correlation.py
@@ -18,5 +18,12 @@ def calculate_pearson(df1, df2):
         min_len = min(len_array1, len_array2)     
         array1 = array1[:min_len]
         array2 = array2[:min_len]
-    return stats.pearsonr(array1, array2)[0]
+        
+    value = stats.pearsonr(array1, array2)
+    print(value)
+    
+    if np.isnan(value[0]):
+        return 999999999
+    else:
+        return value[0]
 

Scripts/Functions/website_functions.py
@@ -114,100 +114,7 @@ def set_calendar(df, calendar):
         calendar = [int(day) for day in calendar]
         # df = df[df.index.weekday.isin(calendar)]
         df[df.index.weekday.isin(calendar)] = np.nan       
-        return df
-    
-def dfs_analysis(dfs, mode, pairwise_comparisons, correlations, pca):
-        
-    for key in dfs:
-        df = dfs[key]
-        dfs[key] = df.dropna()
-    
-    df_keys = list(dfs.keys())
-    df_keys_len = len(df_keys)
-    
-    dic = {}
-    i = 0   
-    for df_key in df_keys:
-        dic[df_key] = i
-        i += 1
-    
-    combos = combinations(df_keys, 2)
-    z = np.zeros(shape=(df_keys_len,df_keys_len))
-    z[z == 0] = 999999999
-       
-    i = 0
-    for combo in combos:
-        i +=1
-        z[dic[combo[0]], dic[combo[1]]] = round(calculate_pearson(dfs[combo[0]], dfs[combo[1]]),3)
-            
-    x = df_keys
-    y = df_keys
-
-    z_list = z.tolist()
-           
-    result = {}
-    result['pearson'] = {'x':x, 'y':y, 'z':z_list}
-    
-    return result
-
-def dfs_analysis_2(dfs, mode, pairwise_comparisons, correlations, pca):
-    
-    for key in dfs:
-        df = dfs[key]
-        dfs[key] = df.dropna()
-    
-    df_keys = list(dfs.keys())
-    
-    combo_results = []
-    
-    combos = combinations(df_keys, 2)
-    
-    i = 0
-    for combo in combos:
-        i +=1
-        combo_results.append([combo[0],combo[1],round(calculate_pearson(dfs[combo[0]], dfs[combo[1]]),3)])
-        
-    distances = calculate_distances(df_keys)      
-    matrixes = calculate_matrix(combo_results, distances)
-    
-    result = {}
-    result['pearson'] = matrixes
-        
-    return result
-    
-def calculate_matrix(combo_results, distances):
-    
-    sensors = list(distances.keys())
-    sensors_len = len(sensors)
-    
-    dic = {}
-    for sensor in sensors:
-        sensors_ordered = distances[sensor]       
-    
-        dic_aux = {}
-        i = 0   
-        for sensor_ordered in sensors_ordered:
-            dic_aux[sensor_ordered] = i
-            i += 1
-        
-        z = np.zeros(shape=(sensors_len,sensors_len))
-        z[z == 0] = 999999999
-        
-        combos = combinations(sensors_ordered, 2)
-        
-        i = 0
-        for combo in combos:
-            i +=1
-            
-            for combo_result in combo_results:
-                if(((combo_result[0] == combo[0]) and (combo_result[1] == combo[1])) 
-                or ((combo_result[0] == combo[1]) and (combo_result[1] == combo[0]))):
-                    z[dic_aux[combo[0]], dic_aux[combo[1]]] = combo_result[2]
-        
-        dic[sensor] = {'x': sensors_ordered, 'y': sensors_ordered, 'z': z.tolist()}
-        
-    return dic
-    
+        return df   
 
 def find_node_by_sensor(sensor):
     
@@ -229,34 +136,7 @@ def find_node_by_sensor(sensor):
             node = sensor['name_long']
             
     return node
-    
-
-def calculate_distances(sensors_id):
-    
-    myGraph = Graph()
-    
-    json_data = get_json()
-    
-    dist = {}
-    
-    for sensor_id in sensors_id:
-        dist_aux = {}
-        for sensor_id_aux in sensors_id:
-            
-            if sensor_id_aux == sensor_id:
-                dist_aux[sensor_id_aux] = 0
-            else:
-                
-                try:
-                    node1 = find_node_by_sensor(json_data['infraquinta'][sensor_id])
-                    node2 = find_node_by_sensor(json_data['infraquinta'][sensor_id_aux])
-                    dist_aux[sensor_id_aux] = myGraph.find_distance(node1, node2)
-                except KeyError:
-                    dist_aux[sensor_id_aux] = 999999999
-                    
-        dist[sensor_id] = list({k: v for k, v in sorted(dist_aux.items(), key=lambda item: item[1])}.keys())
-                          
-    return dist         
+           
 
 def find_distance_sensors(sensor_id1, sensor_id2):
     myGraph = Graph()   
@@ -330,8 +210,9 @@ def dfs_analysis_3(wme, dfs, mode, pairwise_comparisons, correlations, pca):
 
 #dist = calculate_distances(list(map(str, range(0,3))))       
         
-#dfs = get_data('infraquinta', ['0', '1', '2', '4', '5'], "2017-06-01", "2017-06-07", ["0","1","2","3","4","5","6"], 'hours', 1);
-#data = dfs_analysis_infraquinta('infraquinta', dfs, "", "cross type", "", "")
+#dfs = get_data('infraquinta', ['29', '30', '31'], "2017-02-01", "2017-02-07", ["0","1","2","3","4","5","6"], 'hours', 1);
+#print(dfs['29'].describe())
+#data = dfs_analysis_3('infraquinta', dfs, "", "all pairs", "", "")
 #print(data)
 
 